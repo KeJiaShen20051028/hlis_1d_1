27d6079de015a63731890e86b95b3b7f659d3cd4
KeJiaShen20051028 <KeJiaShen20051028@gmail.com>
2022-04-25 09:40:06 +0800

    .

--- !data/2022/0411/get.php
+++ !data/2022/0411/get.php
@@ -1,8 +1,6 @@
 <html>  
    <body>  
-     
-      歡迎 <?php echo $_GET["username"]; ?> </br>  
+     歡迎 <?php echo $_GET["username"]; ?></br>  
      您的年齡: <?php echo $_GET["y"]; ?>  
-  
    </body>  
 </html>  
